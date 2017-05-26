61b56bed92579d5f9fbbaa1dee156da912746ab4
Lygon Bowen-West <l.bowenwest@googlemail.com>
2017-05-26 12:10:17 +0100

    Moved to ROS build system

--- firmware/surgitouch.h
+++ firmware/surgitouch.h
@@ -1,21 +1,7 @@
 #ifndef _SURGITOUCH_H
 #define _SURGITOUCH_H
 
-#define TESTING 1
-
-#define WAIT_TIME 10
-
-#ifdef TESTING
-#define WAIT_TIME 1000
-#include "std_msgs/String.h"
-char buffer[50] = "hello world!\0";
 void message_cb(const std_msgs::String &message);
-#endif
-
-#include "geometry_msgs/Pose2D.h"
-#include "geometry_msgs/Vector3.h"
-
 void force_cb(const geometry_msgs::Vector3 &message);
-
 
 #endif
--- firmware/time.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TIME_H
-#define _TIME_H
-
-#include <stdint.h>
-
-void time_init(void);
-uint32_t time_now(void);
-
-#endif
